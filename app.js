98dd7420aa9edcbc52f3921f525e96a149872b1d
Sarah Currey <curreysarah28@gmail.com>
2018-10-10 23:54:58 -0400

    project one changes

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 window.addEventListener("resize", resize);
-
+let obama = document.querySelector('#project1')
 
 function resize() {
   console.log(window.innerWidth);
   let h1 = document.getElementById('size');
   h1.innerHTML = window.innerWidth
 }
+
+changeProject() {
+ obama.style.backgroundColor = cornflower blue;
+ inner.html= Come Back, Barack. Javascript, HTML, and CSS game
+}
+
+
+add.addEventListener('mouseover', changeProject)
